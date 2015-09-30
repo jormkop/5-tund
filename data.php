716aa47794b7fcad9fbb0d6e87f55f00a5a8a09a
Jorma Kõpp <jormkop@greeny.cs.tlu.ee>
2015-09-30 12:22:28 +0300

    kopeerisin failid

---
+++ data.php
@@ -0,0 +1,24 @@
+<?php
+	require_once("functions.php");
+	//data.php
+	// siia pääseb ligi sisseloginud kasutaja
+	//kui kasutaja ei ole sisseloginud,
+	//siis suuunan data.php lehele
+	if(!isset($_SESSION["logged_in_user_id"])){
+		header("Location: login.php");
+	}
+	
+	//kasutaja tahab välja logida
+	if(isset($_GET["logout"])){
+		//aadressireal on olemas muutuja logout
+		
+		//kustutame kõik session muutujad ja peatame sessiooni
+		session_destroy();
+		
+		header("Location: login.php");
+	}
+	
+?>
+<p>
+	Tere, <?=$_SESSION["logged_in_user_email"];?> 
+	<a href="?logout=1"> Logi välja <a> 
